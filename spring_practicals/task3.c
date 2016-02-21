959df905e65554175bd8fe1e5bf50b738cc25fce
adam <hvfcrazy@hotmail.com>
2016-02-21 10:58:13 +0000

    Task 3 - Ethernet - complete

--- spring_practicals/task3.c
+++ spring_practicals/task3.c
@@ -15,8 +15,8 @@ XEmacLite ether;
 static u8 mac_address[] = {0x00, 0x11, 0x22, 0x33, 0x00, 0x02}; // Remember to change this to *your* MAC address!
 u8 tmit_buffer[XEL_MAX_FRAME_SIZE];
 u8 recv_buffer[XEL_MAX_FRAME_SIZE];
-int currentBufferIndex = 0;
-int MAC = 0x00;
+int currentBufferIndex = 16;
+int MAC;
 int haveMAC = 0;
 int main(){
 	//Initialise the driver
@@ -62,37 +62,44 @@ void sendToEthernet() {
 	for(i = 0; i < 6; i++)
 		*buffer++ = mac_address[i];
 
-	//Write the type/length field
+	//Write the type field
 	*buffer++ = 0x55;
 	*buffer++ = 0xAA;
 
-	//Now fill some data
-	for (i = 0; i < 10; i++) {
-		*buffer++ = (u8) i;
-	}
+	//2 bytes for message length
+	*buffer++ = (currentBufferIndex -16) >> 8;
+	*buffer++ = (currentBufferIndex -16);
 
 	//Send the buffer
 	//The size argument is the data bytes + XEL_HEADER_SIZE which is defined
 	//as the size of the destination MAC plus the type/length field
-	XEmacLite_Send(&ether, tmit_buffer, 10 + XEL_HEADER_SIZE);
+	XEmacLite_FlushReceive(&ether); //Clear any received messages
+	XEmacLite_Send(&ether, tmit_buffer, 2 + (currentBufferIndex -16) + XEL_HEADER_SIZE);
 }
 void recieveFromUART(){
 	char received = XUartLite_RecvByte(XPAR_RS232_DTE_BASEADDR);
 
 	if (received == '\r'){ // If the enter key has been pressed
 		if (haveMAC == 0){
-			strcpy(MAC, tmit_buffer);
+			char * pEnd;
+			char sendersMAC[3];
+			sendersMAC[0] = tmit_buffer[16];
+			sendersMAC[1] = tmit_buffer[17];
+			sendersMAC[2] = '\0';
+			MAC = strtol(sendersMAC, &pEnd,16);
 			haveMAC = 1;
-			return;
 		}
-		sendToEthernet();
-		xil_printf("\n\r");
+		else{
+			sendToEthernet();
+			xil_printf("\n\r");
+			haveMAC = 0;
+		}
 		int i;
 		for(i = 0; i < 64; i++){ // Reset Buffer
 			tmit_buffer[i] = '\0';
 		}
-		currentBufferIndex = 0; // Reset buffer index
-		haveMAC = 0;
+		currentBufferIndex = 16; // Reset buffer index
+
 	}
 	else{ // Store the string being received
 		if (haveMAC == 1){
