19742a3655de4f22596a246b802b6725993ac80f
Adam Taylor <at895@york.ac.uk>
2016-04-27 16:58:56 +0100

    Begin A* implementation

--- summer_open/code/toplevel.cpp
+++ summer_open/code/toplevel.cpp
@@ -1,8 +1,14 @@
 #include "toplevel.h"
 
 //Input data storage
-int32 inputdata[NUMDATA];
-
+int8 distanceMatrix[12][12];
+int6 numberOfWalls;
+int6 numberOfWaypoints;
+int6 worldSize;
+int32 receiveBuffer;
+Node storageGrid[60][60];
+Wall walls[12];
+int8 waypoints[12][2];
 //Prototypes
 
 //Top-level function
@@ -11,11 +17,52 @@ void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output) {
 #pragma HLS RESOURCE variable=output core=AXI4Stream
 #pragma HLS INTERFACE ap_ctrl_none port=return
 
-    //Read in NUMDATA items
-    readloop: for(int i = 0; i < NUMDATA; i++) {
-        inputdata[i] = input.read();
-    }
+	//Read in the data
+	worldSize = input.read();
+	numberOfWalls = input.read();
+	numberOfWaypoints = input.read();
+	wallReadLoop:	for(int wall = 0; wall < numberOfWalls; wall++) {
+		receiveBuffer = input.read();
+		if(receiveBuffer[2] == 0){
+			for(int i = 0; i < receiveBuffer[3]; i++){
+				storageGrid[receiveBuffer[0] + i][receiveBuffer[1]].isWall = 1;
+			}
+		}
+		else{
+			for(int i = 0; i < receiveBuffer[3]; i++){
+				storageGrid[receiveBuffer[0]][receiveBuffer[1] + i].isWall = 1;
+			}
+		}
+	}
 
+	waypointReadLoop: for(int i = 0; i < numberOfWaypoints; i++) {
+		receiveBuffer = input.read();
+		waypoints[i][0] = receiveBuffer[0];
+		waypoints[i][1] = receiveBuffer[1];
+	}
+
+	numberOfWaypointsLoop: for(int waypoint = 0; waypoint < numberOfWaypoints; waypoint++) {
+		storageGrid[waypoints[waypoint][0]][waypoints[waypoint][1]].listMembership = 1;
+		storageGrid[waypoints[waypoint][0]][waypoints[waypoint][1]].cost = 0;
+
+		bool openListEmpty = 0;
+		mainAStarLoop: while(openListEmpty == 0){
+			int12 lowestCost = 0;
+			Node lowestCostNode;
+			openListEmpty = 1;
+			for(int x = 0; x < worldSize; x++){
+				for(int y = 0; y < worldSize; y++){
+					if(storageGrid[x][y].listMembership == 1){
+						openListEmpty = 0;
+						if(storageGrid[x][y].cost < lowestCost){
+							lowestCostNode = storageGrid[x][y];
+						}
+					}
+				}
+			}
+
+		}
+
+	}
 }
-
 
--- summer_open/code/toplevel.h
+++ summer_open/code/toplevel.h
@@ -9,6 +9,25 @@
 //Typedefs
 typedef ap_uint<32> uint32;
 typedef ap_int<32> int32;
+typedef ap_int<1> int1;
+typedef ap_int<2> int2;
+typedef ap_int<4> int4;
+typedef ap_int<8> int8;
+typedef ap_int<6> int6;
+typedef ap_int<12> int12;
+typedef ap_int<32> int32;
+typedef struct Wall{
+   int8 X;
+   int8 Y;
+   int1 direction;
+   int6 length;
+};
+typedef struct Node{
+   int12 cost = 0;
+   int2 listMembership = 0; // 0 is no group, 1 is open and 2 is closed
+   int1 isWall = 0;
+};
+
 
 //Prototypes
 void toplevel(hls::stream<uint32> &input, hls::stream<uint32> &output);
